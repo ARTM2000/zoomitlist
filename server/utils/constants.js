c248439e690fce7751528e82e8d7c4dca72464d4
Alireza Tanoomandian <50656840+ARTM2000@users.noreply.github.com>
2020-08-04 11:28:29 +0430

    Update constants.js

--- server/utils/constants.js
+++ server/utils/constants.js
@@ -3,5 +3,5 @@ const dbPassword = process.env.DB_PASSWORD || "password";
 const dbName = process.env.DB_NAME || "databaseName";
 
 module.exports = {
-  db: `mongodb+srv://${dbUser}:${dbPassword}@cluster0-k82f8.mongodb.net/${dbName}?retryWrites=true&w=majority`,
+  db: `mongodb+srv://${dbUser}:${dbPassword}@cluster0-xxxxx.mongodb.net/${dbName}?retryWrites=true&w=majority`,
 };
